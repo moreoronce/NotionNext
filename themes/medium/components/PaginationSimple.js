39cce15ea11fde1d002722395f007f9259343489
moreoronce <nsy.bati@gmail.com>
2024-08-26 21:45:26 +0800

    push test

--- themes/medium/components/PaginationSimple.js
+++ themes/medium/components/PaginationSimple.js
@@ -13,40 +13,39 @@ const PaginationSimple = ({ page, totalPage }) => {
   const { locale } = useGlobal()
   const router = useRouter()
   const currentPage = +page
+  const showPrev = currentPage !== 1
   const showNext = currentPage < totalPage
   const pagePrefix = router.asPath.split('?')[0].replace(/\/page\/[1-9]\d*/, '').replace(/\/$/, '')
 
   return (
     <div className="my-10 flex justify-between font-medium text-black dark:text-gray-100 space-x-2">
-      <Link
-        href={{
-          pathname:
-            currentPage === 2
-              ? `${pagePrefix}/`
-              : `${pagePrefix}/page/${currentPage - 1}`,
-          query: router.query.s ? { s: router.query.s } : {}
-        }}
-        passHref
-        rel="prev"
-        className={`${
-          currentPage === 1 ? 'invisible' : 'block'
-        } text-center w-full duration-200 px-4 py-2 hover:border-green-500 border-b-2 hover:font-bold`}>
-        ←{locale.PAGINATION.PREV}
-
-      </Link>
-      <Link
-        href={{
-          pathname: `${pagePrefix}/page/${currentPage + 1}`,
-          query: router.query.s ? { s: router.query.s } : {}
-        }}
-        passHref
-        rel="next"
-        className={`${
-          +showNext ? 'block' : 'invisible'
-        } text-center w-full duration-200 px-4 py-2 hover:border-green-500 border-b-2 hover:font-bold`}>
-
-        {locale.PAGINATION.NEXT}→
-      </Link>
+      {showPrev ? (
+        <Link
+          href={{
+            pathname:
+              currentPage === 2
+                ? `${pagePrefix}/`
+                : `${pagePrefix}/page/${currentPage - 1}`,
+            query: router.query.s ? { s: router.query.s } : {}
+          }}
+          passHref
+          rel="prev"
+          className="block text-center w-full duration-200 px-4 py-2 hover:border-green-500 border-b-2 hover:font-bold">
+          ←{locale.PAGINATION.PREV}
+        </Link>
+      ) : <div></div>}
+      {showNext ? (
+        <Link
+          href={{
+            pathname: `${pagePrefix}/page/${currentPage + 1}`,
+            query: router.query.s ? { s: router.query.s } : {}
+          }}
+          passHref
+          rel="next"
+          className="block text-center w-full duration-200 px-4 py-2 hover:border-green-500 border-b-2 hover:font-bold">
+          {locale.PAGINATION.NEXT}→
+        </Link>
+      ) : <div></div>}
     </div>
   )
 }
