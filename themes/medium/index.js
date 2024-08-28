ce19f2e3bdafa0e16f8ac59a8dfd8a45fb200e31
moreoronce <nsy.bati@gmail.com>
2024-08-28 10:50:08 +0800

    upate

--- themes/medium/index.js
+++ themes/medium/index.js
@@ -273,6 +273,7 @@ const LayoutArchive = props => {
   const { archivePosts } = props
   return (
         <>
+        <h1 class="pt-16 text-3xl dark:text-gray-300">归档 Archive</h1>
             <div className="mb-10 pb-20 md:py-12 py-3  min-h-full">
                 {Object.keys(archivePosts)?.map(archiveTitle => <BlogArchiveItem key={archiveTitle} archiveTitle={archiveTitle} archivePosts={archivePosts} />
                 )}
