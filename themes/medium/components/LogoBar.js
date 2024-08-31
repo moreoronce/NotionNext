04cf5f5a9dda9308d01a696fc2284bfadc9afb66
moreoronce <nsy.bati@gmail.com>
2024-08-31 21:04:01 +0800

    add /page/ h1 tag

--- themes/medium/components/LogoBar.js
+++ themes/medium/components/LogoBar.js
@@ -5,10 +5,11 @@ import { siteConfig } from '@/lib/config'
 export default function LogoBar (props) {
   const router = useRouter();
   const isHomePage = router.pathname === '/';
+  const isPageNumber = router.pathname.startsWith('/page/');
 
   return (
     <div id='top-wrapper' className='w-full flex items-center '>
-      {isHomePage ? (
+      {isHomePage || isPageNumber ? (
         <h1>
           <Link href='/' className='text-md md:text-xl dark:text-gray-200'>
             {siteConfig('TITLE')}
