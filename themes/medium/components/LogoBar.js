ac05ed35fbb1fc3d9733252749f947a1b890dcf5
moreoronce <nsy.bati@gmail.com>
2024-08-31 20:58:16 +0800

    add h1 to logobar

--- themes/medium/components/LogoBar.js
+++ themes/medium/components/LogoBar.js
@@ -1,12 +1,24 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { siteConfig } from '@/lib/config'
 
 export default function LogoBar (props) {
+  const router = useRouter();
+  const isHomePage = router.pathname === '/';
+
   return (
     <div id='top-wrapper' className='w-full flex items-center '>
+      {isHomePage ? (
+        <h1>
           <Link href='/' className='text-md md:text-xl dark:text-gray-200'>
             {siteConfig('TITLE')}
           </Link>
+        </h1>
+      ) : (
+        <Link href='/' className='text-md md:text-xl dark:text-gray-200'>
+          {siteConfig('TITLE')}
+        </Link>
+      )}
     </div>
   );
 }
