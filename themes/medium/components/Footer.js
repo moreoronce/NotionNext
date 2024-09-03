327ea1199e9984ff478f74abbe39f4eefaa7e070
moreoronce <nsy.bati@gmail.com>
2024-09-03 22:43:37 +0800

    update footer

--- themes/medium/components/Footer.js
+++ themes/medium/components/Footer.js
@@ -16,7 +16,6 @@ const Footer = ({ title }) => {
       </div>
       <div>
         <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={siteConfig('LINK')} className='underline font-bold text-gray-500 dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br/>
-        {title}
         <br/>
         <span className='text-xs font-serif'>Powered by <a href='https://github.com/tangly1024/NotionNext' className='underline text-gray-500 dark:text-gray-300' rel='nofollow'>NotionNext {siteConfig('VERSION')}</a>.</span></span>
       </div>
