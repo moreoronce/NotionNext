3d896a2d80c17db6c67292aa0890ca5c0b12aadb
moreoronce <nsy.bati@gmail.com>
2024-09-06 14:49:15 +0800

    update footer

--- themes/medium/components/Footer.js
+++ themes/medium/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = ({ title }) => {
     <footer className='z-10 dark:bg-hexo-black-gray flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm p-6 relative'>
       <br/>
       <div>
-        <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={siteConfig('LINK')} className='underline font-bold text-gray-500 dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br/>
+        <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={siteConfig('LINK')} className='underline font-bold text-gray-500 dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>. On Vercel<br/>
         <span className='text-xs font-serif'>Powered by <a href='https://github.com/tangly1024/NotionNext' className='underline text-gray-500 dark:text-gray-300' rel='nofollow'>NotionNext {siteConfig('VERSION')}</a>. Support IPv4 & IPv6</span></span>
       </div>
     </footer>
