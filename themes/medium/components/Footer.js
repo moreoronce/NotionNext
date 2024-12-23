fb514b4a3d368f93c62b796a83042d7ff7e53213
Yawata <nsy.bati@gmail.com>
2024-12-23 15:36:20 +0800

    remove busuanzi

--- themes/medium/components/Footer.js
+++ themes/medium/components/Footer.js
@@ -31,14 +31,6 @@ const Footer = ({ title }) => {
           </>
         )}
         <BeiAnGongAn />
-        <span className='hidden busuanzi_container_site_pv'>
-          <i className='fas fa-eye' />
-          <span className='px-1 busuanzi_value_site_pv'> </span>
-        </span>
-        <span className='pl-2 hidden busuanzi_container_site_uv'>
-          <i className='fas fa-users' />
-          <span className='px-1 busuanzi_value_site_uv'> </span>
-        </span>
         <span className='text-xs font-serif'>
           Powered by 
           <a
