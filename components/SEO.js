b90b1eb3098aa766ddb2318e73a208e7d0c068f8
Yawata <nsy.bati@gmail.com>
2024-12-30 15:02:55 +0800

    尝试修复

--- components/SEO.js
+++ components/SEO.js
@@ -50,6 +50,7 @@ const SEO = props => {
   }
   const title = meta?.title || siteConfig('TITLE')
   const description = meta?.description || `${siteInfo?.description}`
+  const subdescription = siteConfig('SUBDESCRIPTION')
   const type = meta?.type || 'website'
   const lang = siteConfig('LANG').replace('-', '_') // Facebook OpenGraph 要 zh_CN 這樣的格式才抓得到語言
   const category = meta?.category || siteConfig('KEYWORDS') // section 主要是像是 category 這樣的分類，Facebook 用這個來抓連結的分類
